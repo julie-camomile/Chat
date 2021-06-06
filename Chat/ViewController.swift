16eb6cdf936b56c64254ac412e45682cd2b688f8
Julia Alekseeva <julika.orekhova@gmail.com>
2021-06-06 23:22:45 +0300

    Initial Commit

---
+++ Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by y.o.alekseeva on 06.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
